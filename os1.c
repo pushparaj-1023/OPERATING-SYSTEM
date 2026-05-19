6cc73d4e4d0fe2b4e366be9c9f4a33a144b9e2a4
pushparaj-1023 <pushparajd870@gmail.com>
2026-05-19 19:45:12 +0530

    ex.1

---
+++ os1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int main()
+{
+pid_t pid;
+pid = fork(); // create child process
+if (pid < 0) {
+printf("Fork failed\n");
+}
+else if (pid == 0) {
+printf("Child Process\n");
+printf("Child PID = %d\n", getpid());
+printf("Parent PID = %d\n", getppid());
+}
+else
+{
+wait(NULL); // wait for child to finish
+printf("Parent Process\n");
+printf("Parent PID = %d\n", getpid());
+printf("Child PID = %d\n", pid);
+}
+return 0;
+}
